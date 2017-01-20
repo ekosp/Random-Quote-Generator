8ea4acd680c2c2a6e97e77c2b804c5a9b728e326
praneetsharma <praneet.223@gmail.com>
2017-01-20 21:37:48 +0530

    UI improvement
    1. quote and author being shown
    2. twitter share and new quote buttons in place. [TODO: Complete their
    functionality]

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   var backgroundColors = ["#44AF69", "#F8333C", "#FCAB10", "#2B9EB3", "#2A2D34"];
-  var foregroundColors = ["white", "black", "white", "black", "black"];
-
+  var foregroundColors = ["white", "white", "white", "white", "white"];
+  var MAX_QUOTE_LEN = 180;
   var randomQuoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
   function getRandomBackgroundColor() {
@@ -28,23 +28,50 @@ $(document).ready(function() {
     return colorCombo;
   }
 
-  // Initializing foreground and background color
-  var initColorCombo = getRandomForeBackColorCombo();
-  $("body").css("background-color", initColorCombo.backColor);
-  $(".quote-placeholder").css("color", initColorCombo.foreColor);
-
   // Initial random quote population
   /*$.getJSON("https://crossorigin.me/https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(val) {
     $(".quote").html(val[0].content);
   });*/
 
-  $.ajax( {
-    url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-    success: function(data) {
-      var firstQuote = data.shift();
-      $(".quote").html(firstQuote.content);
-    },
-    cache: false
-  });
+  function requestQuote () {
+    var isSuccess = true;
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function(data) {
+        var firstQuote = data.shift();
+        if (firstQuote.content.length > MAX_QUOTE_LEN) {
+          isSuccess = false;
+        } else {
+          $(".quote-content-row").html(firstQuote.content);
+          $(".quote-author").html(firstQuote.author);
+          isSuccess = true;
+        }
+      },
+      cache: false
+    });
+    return isSuccess;
+  }
+
+
+  /* ====== DEFAULT-START =====*/
+
+  // Initializing foreground and background color
+  var initColorCombo = getRandomForeBackColorCombo();
+  $("body").css("background-color", initColorCombo.backColor);
+  $(".quote-placeholder").css("color", initColorCombo.foreColor);
+
+  $(".quote-content-row").css("color", initColorCombo.backColor);
+  $(".quote-content-row").css("background-color", initColorCombo.foreColor);
+  $(".quote-author").css("color", initColorCombo.backColor);
+  $(".quote-content-parent").css("background-color", initColorCombo.backColor);  
+
+  $(".btn-twitter").css("background-color", initColorCombo.backColor);
+  $(".tweet-quote").css("background-color", initColorCombo.backColor);
+  $(".fa-twitter").css("color", initColorCombo.foreColor);
+
+  // requesting initial code
+  while(! requestQuote());
+
+  /* ====== DEFAULT-END =====*/
 
 });
