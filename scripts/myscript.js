75b77bd728e40cf416b51a2ecc511b523a417c16
praneetsharma <praneet.223@gmail.com>
2017-01-21 13:13:22 +0530

    Tweet, New quote buttons working, and some general functionality changes
    1. Ability to tweet the random quote shown on screen.
    2. "New quote" button now shows a new quote each time it is pressed.
    3. fa-quote icon being shown along with quote content.
    
    Functionality Issues
    1. Clicking "new quote" button doesn't return a new quote some times.
    2. The quote to tweet is not being encoded properly for twitter to
    undestand. Encoding doesn't working for special chars like single quote,
    double quote.
    
    TODO
    1. Many cosmetic changes
    a. Shadow for quote box
    b. Better "new quote" button
    c. etc.
    2. Some functionality changes

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
   var backgroundColors = ["#44AF69", "#F8333C", "#FCAB10", "#2B9EB3", "#2A2D34"];
   var foregroundColors = ["white", "white", "white", "white", "white"];
-  var MAX_QUOTE_LEN = 180;
+  var MAX_QUOTE_LEN = 120; // Making sure the whole quote can be shared on twitter
+  var SPACE = " ";
+  var TWITTER_HASTAG = "QuoteOfTheDay";
+  var DOT = ".";
+
+  var quoteContent = "";
+  var quoteAuthor = "";
   var randomQuoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+  var faQuoteStartRight = "<i class='fa fa-quote-right' aria-hidden='true'></i>";
+  var faQuoteStartLeft = "<i class='fa fa-quote-left' aria-hidden='true'></i>";
 
   function getRandomBackgroundColor() {
     var idx = Math.floor(Math.random() * backgroundColors.length);
@@ -28,22 +37,51 @@ $(document).ready(function() {
     return colorCombo;
   }
 
+  function changeColorsOfScreen (colorCombo) {
+    $("body").css("background-color", colorCombo.backColor);
+    $(".quote-placeholder").css("color", colorCombo.foreColor);
+
+    $(".quote-content-row").css("color", colorCombo.backColor);
+    $(".quote-content-row").css("background-color", colorCombo.foreColor);
+    $(".quote-author").css("color", colorCombo.backColor);
+    $(".quote-content-parent").css("background-color", colorCombo.backColor);
+
+    $(".btn-twitter").css("background-color", colorCombo.backColor);
+    $(".tweet-quote").css("background-color", colorCombo.backColor);
+    $(".fa-twitter").css("color", colorCombo.foreColor);
+  }
+
   // Initial random quote population
   /*$.getJSON("https://crossorigin.me/https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(val) {
     $(".quote").html(val[0].content);
   });*/
 
-  function requestQuote () {
+  function requestQuote (colorCombo) {
     var isSuccess = true;
+    var backColor = colorCombo.backColor;
+    var foreColor = colorCombo.foreColor;
     $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      url: randomQuoteUrl,
       success: function(data) {
         var firstQuote = data.shift();
         if (firstQuote.content.length > MAX_QUOTE_LEN) {
           isSuccess = false;
         } else {
-          $(".quote-content-row").html(firstQuote.content);
-          $(".quote-author").html(firstQuote.author);
+          quoteContent = firstQuote.content
+            .replace("<p>","") // removes <p> attribute
+            .replace("</p>","") // removes </p> attribute
+            .replace("\n",""); // removes newline char
+          quoteContent = quoteContent.substring(0, quoteContent.length - 1); // removes 1 unnecessary space from the very end
+
+          var quoteContent_ = faQuoteStartLeft + SPACE + quoteContent + SPACE + faQuoteStartRight; // adding start and end quote marks to quote content
+
+          $(".fa-quote-left").css("background-color", backColor);
+          $(".fa-quote-right").css("background-color", foreColor);
+
+          $(".quote-content-row").html(quoteContent_);
+          $(".quote-author").html(firstQuote.title);
+
+          quoteAuthor = firstQuote.title;
           isSuccess = true;
         }
       },
@@ -52,26 +90,38 @@ $(document).ready(function() {
     return isSuccess;
   }
 
+  function requestQuoteTillNecessary () {
+    var colorCombo = getRandomForeBackColorCombo();
+    changeColorsOfScreen(colorCombo);
+    while(! requestQuote(colorCombo));
+  }
+
+  function tweetQuote () {
+    // build tweet URL
+    var tweetUrl = 'https://twitter.com/intent/tweet?hashtags=' +
+      TWITTER_HASTAG +
+      '&text=' +
+      encodeURIComponent('"' + quoteContent + '"' + SPACE + quoteAuthor);
+
+    // open URL in different window
+    window.open(tweetUrl);
+  }
 
   /* ====== DEFAULT-START =====*/
 
   // Initializing foreground and background color
   var initColorCombo = getRandomForeBackColorCombo();
-  $("body").css("background-color", initColorCombo.backColor);
-  $(".quote-placeholder").css("color", initColorCombo.foreColor);
 
-  $(".quote-content-row").css("color", initColorCombo.backColor);
-  $(".quote-content-row").css("background-color", initColorCombo.foreColor);
-  $(".quote-author").css("color", initColorCombo.backColor);
-  $(".quote-content-parent").css("background-color", initColorCombo.backColor);  
-
-  $(".btn-twitter").css("background-color", initColorCombo.backColor);
-  $(".tweet-quote").css("background-color", initColorCombo.backColor);
-  $(".fa-twitter").css("color", initColorCombo.foreColor);
-
-  // requesting initial code
-  while(! requestQuote());
+  // requesting initial quote
+  requestQuoteTillNecessary(initColorCombo);
 
   /* ====== DEFAULT-END =====*/
+
+
+  /* ====== BUTTON CLICK ACTIONS ==== */
+  $(".new-quote-btn").click(requestQuoteTillNecessary);
+
+  $("#tweet-quote").click(tweetQuote);
+
 
 });
